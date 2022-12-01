d8957a0c461e055df90e0c13e581129f8afe651f
Manav Penubaku <43569817+altairbaku@users.noreply.github.com>
2022-12-01 05:49:56 +0000

    solved day 1

--- src/day1.py
+++ src/day1.py
@@ -2,8 +2,16 @@ with open('../input/day1.txt') as f:
     lines = f.readlines()
 
 d1_input = []
+cal_list  = []
+sum_cal = 0
 for line in lines:
-    d1_input.append(int(line))
+    if line != "\n":
+        sum_cal += int(line)
+    else:
+        cal_list.append(sum_cal)
+        sum_cal = 0
+cal_list.append(sum_cal)
 
-
-
+cal_list.sort(reverse=True)
+print("Part 1 : ",cal_list[0])
+print("Part 2 : ",sum(cal_list[0:3]))
