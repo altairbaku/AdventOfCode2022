816cdf792158dec358f3d31668ba4586db070c21
Manav Penubaku <43569817+altairbaku@users.noreply.github.com>
2022-12-02 08:52:19 +0000

    solved day 2 part 2

--- src/day2.py
+++ src/day2.py
@@ -2,22 +2,26 @@ with open('../input/day2.txt') as f:
     lines = f.readlines()
 
 score = 0
+score_p2 = 0
 for line in lines:
     plays = line.split(" ")
     player_1 = ord(plays[0].strip())-64
     player_2 = ord(plays[1].strip())-87
-    if player_1 == player_2:
+    play_diff = player_2 - player_1
+    if play_diff == 0:
         score += 3 + player_2
-    elif player_2 > player_1:
-        if player_2 == 3 and player_1 == 1:
-            score += player_2
-        else:
-            score += 6 + player_2
+    elif play_diff == 1 or play_diff == -2:
+        score += 6 + player_2
     else:
-        if player_1 == 3 and player_2 == 1:
-            score += 6 + player_2
-        else:
-            score += player_2
+        score += player_2
 
-print(score)
-            
+    if player_2 == 1:
+        score_p2 += (player_1 - 1) if player_1 != 1 else 3
+    elif player_2 == 2:
+        score_p2 += player_1 + 3
+    else:
+        score_p2 += (player_1 + 1) if player_1 != 3 else 1
+        score_p2 += 6
+
+print("Part 1 : ",score)     
+print("Part 2 : ",score_p2)
