8c789931601a1138e807b22a93534111dae40cea
ManavPenubaku <manav.prasad.27@gmail.com>
2022-12-03 08:43:24 +0100

    solved day 3

--- src/day3.py
+++ src/day3.py
@@ -9,16 +9,22 @@ for x in keys:
     dict[x] = n
     n+=1
 
-sum_items = 0
+sum_p1 = 0
 for rucksack in rucksacks:
     n_half  = int(len(rucksack)/2)
-    compartment_1 = set(rucksack[1:n_half])
-    compartment_2 = set(rucksack[n_half::])
+    compartment_1 = set(rucksack[0:n_half])
+    compartment_2 = set(rucksack[n_half:])
     for item in compartment_1:
         if item in compartment_2:
-            print(item,dict[item])
-            sum_items += dict[item]
+            sum_p1 += dict[item]
             break
 
-print(sum_items)
+sum_p2 = 0
+for n in range(0,len(rucksacks),3):
+    for item in set(rucksacks[n]):
+        if item in set(rucksacks[n+1].strip()) and item in set(rucksacks[n+2].strip()):
+            sum_p2+=dict[item]
+
+print("Part 1 : ",sum_p1)
+print("Part 2 : ",sum_p2)
 
