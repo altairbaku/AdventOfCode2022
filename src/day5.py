257de5bbbd0a7738bb5847f244eaec7bc1db8a97
Manav Penubaku <43569817+altairbaku@users.noreply.github.com>
2022-12-06 20:34:35 +0000

    solve day 5

--- src/day5.py
+++ src/day5.py
@@ -1,28 +1,41 @@
 import re
 from collections import defaultdict
+import copy
   
-with open('../input/day5_example.txt') as f:
+with open('../input/day5.txt') as f:
     lines = f.readlines()
 
-crate_dict = defaultdict(list)
+crate_dict_p1 = defaultdict(list)
 
 while lines[0] != "\n":
     container_regex = re.compile("[[]([A-Z])[]]")
     matches = container_regex.finditer(lines[0])
     lines.remove(lines[0])
     for hit in matches:
-        crate_dict[int(hit.start()/4) + 1].append(hit.group()[1])
+        crate_dict_p1[int(hit.start()/4) + 1].append(hit.group()[1])
 
-print(crate_dict)
+crate_dict_p2 = copy.deepcopy(crate_dict_p1)
 
 for line in lines[1::]:
     instructions = [int(s) for s in line.split() if s.isdigit()]
-    crate_dict[instructions[2]] = crate_dict[instructions[1]][0:instructions[0]] + crate_dict[instructions[2]]
-    del crate_dict[instructions[1]][0:instructions[0]]
-    print(crate_dict)
+    stack = crate_dict_p1[instructions[1]][0:instructions[0]]
+    stack.reverse()
+    crate_dict_p1[instructions[2]] = stack + crate_dict_p1[instructions[2]]
+    del crate_dict_p1[instructions[1]][0:instructions[0]]
+    crate_dict_p2[instructions[2]] = crate_dict_p2[instructions[1]][0:instructions[0]] + crate_dict_p2[instructions[2]]
+    del crate_dict_p2[instructions[1]][0:instructions[0]]
 
-for n in range(len(crate_dict)):
-    print(crate_dict[n+1][0])
+p1 = ''
+p2 = ''
+
+for n in range(len(crate_dict_p1)):
+    p1 += crate_dict_p1[n+1][0]
+    p2 += crate_dict_p2[n+1][0]
+
+print("Part 1 : ",p1)
+print("Part 2 : ",p2)
+
+
 
         
 
