245a7335c4897a471f5f4c4c37bcba0fa53a1cd7
ManavPenubaku <manav.prasad.27@gmail.com>
2022-12-07 21:30:49 +0100

    solved day 7 part 2

--- src/day7.py
+++ src/day7.py
@@ -3,30 +3,28 @@ with open('../input/day7.txt') as f:
 
 def directory_size_list(filesystem,index):
     directory_size = 0
-    p1_sol = 0
+    directory_sizes = []
     filesystem_len = len(filesystem)
     while index != filesystem_len:
         line = filesystem[index]
         index += 1
         if line[2:4] == "cd" and line[5:] != "..\n":
-            sub_directory_size,p1_sub_size,index = directory_size_list(filesystem,index)
-            directory_size += sub_directory_size
-            p1_sol += p1_sub_size
+            index,sub_size_list,sub_size = directory_size_list(filesystem,index)
+            directory_sizes.extend(sub_size_list)
+            directory_size += sub_size
         elif line[2:4] == "cd" and line[5:] == "..\n":
-            if directory_size <= 100000:
-                p1_sol += directory_size
-            print(directory_size)
-            return directory_size,p1_sol,index
+            directory_sizes.append(directory_size)
+            return index,directory_sizes,directory_size
         elif line[0:3] != "dir" and line[2:4] != "ls":
             directory_size += int(line.split()[0])
     
-    print(directory_size)
-    if directory_size <= 100000:
-        p1_sol += directory_size
-    return directory_size,p1_sol,index
+    directory_sizes.append(directory_size)
+    return index,directory_sizes,directory_size
 
-total_size,p1_size,index = directory_size_list(lines,0)
+index,size_list,total_size = directory_size_list(lines,0)
 
-print(total_size)
-print(p1_size)
+p1_list = filter(lambda x: x <= 100000,size_list)
+p2_list = filter(lambda y: y >= max(size_list)-40000000,size_list)
+print(sum(p1_list))
+print(sorted(p2_list)[0])
 
