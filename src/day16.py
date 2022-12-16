4ac6715276978b53528b87e85ad406ca22c3a15a
Manav Penubaku <43569817+altairbaku@users.noreply.github.com>
2022-12-16 06:12:09 +0000

    parse input of day 16

---
+++ src/day16.py
@@ -0,0 +1,13 @@
+import re
+
+with open('../input/day16.txt') as f:
+    valve_scan = f.readlines()
+
+valve_flows = dict()
+valve_tunnels = dict()
+for scan in valve_scan:
+    split_scan = scan.split(";")
+    valve_info = re.search(r'Valve ([A-Z]+) has flow rate=(\d+)',split_scan[0])
+    cur_valve = valve_info.group(1)
+    valve_flows[cur_valve] = int(valve_info.group(2))
+    valve_tunnels[cur_valve] = re.findall(r'([A-Z]+)',split_scan[1])
