c9534055d50f4a347dcdf65a5c14efd856ee42bf
Manav Penubaku <43569817+altairbaku@users.noreply.github.com>
2022-12-20 14:17:05 +0000

    solved day 20

--- src/20.py
+++ src/20.py
@@ -1,16 +1,33 @@
 from copy import deepcopy
-with open('../input/20_example.txt') as f:
+from collections import deque
+
+with open('../input/20.txt') as f:
     lines = f.readlines()
-    
+file_p1 = deque([(int(value.strip()),index) for index,value in enumerate(lines)])
 
-initial_file = [int(line.strip()) for line in lines]
+def move(file,position):
+    index = file.index(position)
+    file.rotate(-index)
+    elem = file.popleft()
+    file.rotate(-position[0])
+    file.append(elem)
+    return file
 
-mixed_file = deepcopy(initial_file)
-for position in initial_file:
-    index = mixed_file.index(position)
-    new_index = (index + position) % len(initial_file)
-    mixed_file.remove(position)
-    mixed_file = mixed_file[:new_index] + [position] + mixed_file[new_index:]
-    print(mixed_file)
+mixed_file = deepcopy(file_p1)
 
-print(mixed_file)
+for position in file_p1:
+    mixed_file = move(mixed_file,position)
+
+mixed_file.rotate(-mixed_file.index((0,4094)))
+print("Part 1 : ",mixed_file[1000][0]+mixed_file[2000][0]+mixed_file[3000][0])
+
+decryption_key = 811589153
+file_p2 = deque([(int(value.strip()) * decryption_key,index) for index,value in enumerate(lines)])
+mixed_file_p2 = deepcopy(file_p2)
+
+for _ in range(10):
+    for position in file_p2:
+        mixed_file_p2 = move(mixed_file_p2,position)
+
+mixed_file_p2.rotate(-mixed_file_p2.index((0,4094)))
+print("Part 2 : ",mixed_file_p2[1000][0]+mixed_file_p2[2000][0]+mixed_file_p2[3000][0])
