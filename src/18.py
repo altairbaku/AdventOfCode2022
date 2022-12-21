8d2706e6b88cc38a7bc2a27eabe6e353c2333390
ManavPenubaku <manav.prasad.27@gmail.com>
2022-12-21 15:30:43 +0100

    solved day 18 part 2

--- src/18.py
+++ src/18.py
@@ -1,3 +1,4 @@
+from collections import deque
 with open('../input/18.txt') as f:
     lava_cube_coordinates = f.readlines()
 
@@ -7,51 +8,57 @@ for coord in lava_cube_coordinates:
 
 lava_droplets = list(lava_set)
 lava_droplets.sort()
-print(lava_droplets)
+x_max = max(lava_droplets,key = lambda x: x[0])[0] + 1
+x_min = min(lava_droplets,key = lambda x: x[0])[0] - 1
+y_max = max(lava_droplets,key = lambda x: x[1])[1] + 1
+y_min = min(lava_droplets,key = lambda x: x[1])[1] - 1
+z_max = max(lava_droplets,key = lambda x: x[2])[2] + 1
+z_min = min(lava_droplets,key = lambda x: x[2])[2] - 1
+
 
 def get_neighbors(cube_coord):
-    neighbors = {(cube_coord[0]+1,cube_coord[1],cube_coord[2]),
-    (cube_coord[0]-1,cube_coord[1],cube_coord[2]),
-    (cube_coord[0],cube_coord[1]+1,cube_coord[2]),
-    (cube_coord[0],cube_coord[1]-1,cube_coord[2]),
-    (cube_coord[0],cube_coord[1],cube_coord[2]+1),
-    (cube_coord[0],cube_coord[1],cube_coord[2]-1)}
+    x = cube_coord[0]
+    y = cube_coord[1]
+    z = cube_coord[2]
+    if x > x_min and x < x_max and y > y_min and y < y_max and z > z_min and z < z_max:
+        neighbors = {(x+1,y,z),
+        (x-1,y,z),
+        (x,y+1,z),
+        (x,y-1,z),
+        (x,y,z+1),
+        (x,y,z-1)}
+    else:
+        neighbors = set()
     return neighbors
 
-def interior_check(cube_coord, lava_set):
-        surface_x = cube_coord[0]
-        surface_y = cube_coord[1]
-        surface_z = cube_coord[2]
-        back = list(filter(lambda x:x[0] == surface_x and x[1] == surface_y and x[2] < surface_z,lava_set)) == []
-        front = list(filter(lambda x:x[0] == surface_x and x[1] == surface_y and x[2] > surface_z,lava_set)) == []
-        up = list(filter(lambda x:x[0] == surface_x and x[1] < surface_y and x[2] == surface_z,lava_set)) == []
-        down = list(filter(lambda x:x[0] == surface_x and x[1] > surface_y and x[2] == surface_z,lava_set)) == []
-        left = list(filter(lambda x:x[0] < surface_x and x[1] == surface_y and x[2] == surface_z,lava_set)) == []
-        right = list(filter(lambda x:x[0] > surface_x and x[1] == surface_y and x[2] == surface_z,lava_set)) == []
-        return (back or front or up or down or left or right)
+def flood_fill(cube_coord, lava_set, air_pockets):
+    visited = set()
+    to_visit = deque([cube_coord])
+    while to_visit:
+        current_coord = to_visit.popleft()
+        neighbors = get_neighbors(current_coord)
+        if neighbors:
+            for neighbor in neighbors:
+                if neighbor not in lava_set and neighbor not in visited and neighbor not in to_visit:
+                    to_visit.append(neighbor)
+        else:
+            return 1,air_pockets
+        visited.add(current_coord)
+    new_set = air_pockets.union(visited)
+    return 0,new_set
 
 exposed_surface_area = 0
 exposed_surfaces_p2 = 0
+air_pockets = set()
 for lava in lava_set:
     neighbors = get_neighbors(lava)
     exposed_surfaces = neighbors ^ (neighbors & lava_set)
     exposed_surface_area += len(exposed_surfaces)
     for exposed_surface in exposed_surfaces:
-        # exposed_surfaces_p2 += 1 if interior_check(exposed_surface,lava_set) else 0
-        neighbors_2 = get_neighbors(exposed_surface)
-        if len(neighbors_2 & lava_set) < 4:
-            exposed_surfaces_p2+=1
+        if exposed_surface not in air_pockets:
+            exterior,air_pockets = flood_fill(exposed_surface,lava_set,air_pockets)
+            exposed_surfaces_p2 += exterior
 
-
-
-# air_pockets = 0
-# for exposed_surface in exposed_surfaces_set:
-#     neighbors = get_neighbors(exposed_surface)
-#     air_pockets = neighbors & exposed_surfaces_set
-#     air_pockets_2 = neighbors & lava_set
-#     print(air_pockets)
-#     print(air_pockets_2)
-
-print(exposed_surface_area)
-print(exposed_surfaces_p2)
+print("Part 1 : ",exposed_surface_area)
+print("Part 2 : ",exposed_surfaces_p2)
 
