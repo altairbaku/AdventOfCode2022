8d842edfccfc1358da0dbad067d2a532196a33b6
Manav Penubaku <43569817+altairbaku@users.noreply.github.com>
2022-12-21 10:46:32 +0000

    solved day 21 part 2

--- src/21.py
+++ src/21.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 with open('../input/21.txt') as f:
     lines = f.readlines()
 
@@ -7,16 +8,41 @@ for line in lines:
     monkey_yells[split_line[0]] = split_line[1]
 
 def eval_monkey(yells_dict,monkey):
-    print(monkey)
     try:
-        yell = eval(yells_dict[monkey])
+        yell = int(eval(yells_dict[monkey]))
     except:
         split_equation = yells_dict[monkey].split()
-        print(split_equation)
         yells_dict[monkey] = yells_dict[monkey].replace(split_equation[0],str(eval_monkey(yells_dict, split_equation[0])))
         yells_dict[monkey] = yells_dict[monkey].replace(split_equation[2],str(eval_monkey(yells_dict, split_equation[2])))
         yell = int(eval(yells_dict[monkey]))
     return yell
 
-p1 = eval_monkey(monkey_yells,'root')
+# def update_equation(eq):
+#     eq_new = []
+#     eq_new.append('humn')
+#     left = eq[0]
+#     right = eq[1].split()
+#     if right[1] == '+':
+#         eq_new.append()
+
+monkey_yells_p1 = deepcopy(monkey_yells)
+p1 = eval_monkey(monkey_yells_p1,'root')
 print(p1)
+components = monkey_yells_p1['root'].split()
+print(list(filter(lambda elem: 'humn' in elem[1], monkey_yells.items())))
+
+split_root = monkey_yells['root'].split()
+monkey_yells['root'] = monkey_yells['root'].replace(split_root[1],'-')
+
+monkey_yells['humn'] = 'lpjj + pvrr'
+humn_value = 3887609739999
+while True:
+    temp_monkey_yells = deepcopy(monkey_yells)
+    temp_monkey_yells['humn'] = str(humn_value)
+    root_yell = eval_monkey(temp_monkey_yells,'root')
+    if root_yell == 0:
+        print(humn_value)
+        break
+    else:
+        humn_value += 1
+
